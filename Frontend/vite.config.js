770afd4b12d60d44423c4e7a2f8f216c91a01156
AJFShuvo <ajfshuvo98@gmail.com>
2025-04-12 21:49:53 +0600

    test : App.test.jsx(App.jsx)

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,           // Allows using `test`, `expect` without imports
+    environment: 'jsdom',   // Simulates a browser DOM for React testing
+    setupFiles: ['./vitest.setup.js'], // Loads the setup file
+  },
 })
